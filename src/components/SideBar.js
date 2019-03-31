30fc64a1c760a2ef779715aa92dc3e91a59a6ba2
Nhan <hnngo@ntu.edu.sg>
2019-03-31 23:05:44 +0800

    Adding Icon clicking features on large screen

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,7 +1,92 @@
 import React, { Component } from 'react';
 import data from '../data.json';
+import {
+  SIDE_HOME,
+  SIDE_CALENDAR,
+  SIDE_SETTING,
+  SIDE_QUESTION
+} from '../constants';
 
 export default class SideBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      reloadImg: false,
+      subSideType: undefined
+    };
+  }
+
+  removeAllBgSilver() {
+    const targetList = ["fa-home", "fa-calendar-check", "fa-cog", "fa-question-circle"];
+
+    targetList.forEach((item) => {
+      document.querySelector(`.${item}`).classList.remove("bg-silver");
+    });
+  }
+
+  toggleSideAndEffectIcon(type, target) {
+
+    if (this.state.subSideType === type) {
+      target.classList.remove("bg-silver");
+      this.setState({ subSideType: undefined });
+    } else {
+      this.removeAllBgSilver();
+      target.classList.add("bg-silver");
+      this.setState({ subSideType: type });
+    }
+  }
+
+  handleClickIcon(e) {
+    // Get the specific type of icon
+    const type = e.target;
+
+    switch (type.classList[4]) {
+      case "fa-home":
+        this.toggleSideAndEffectIcon(SIDE_HOME, type);
+        return;
+      case "fa-calendar-check":
+        this.toggleSideAndEffectIcon(SIDE_CALENDAR, type);
+        return;
+      case "fa-cog":
+        this.toggleSideAndEffectIcon(SIDE_SETTING, type);
+        return;
+      case "fa-question-circle":
+        this.toggleSideAndEffectIcon(SIDE_QUESTION, type);
+        return;
+      default:
+        return;
+    }
+  }
+
+  renderSubSideBar() {
+    if (this.state.subSideType) {
+      return (
+        <div className="sidebar-sub-container d-flex ">
+          <div className="sidebar-sub-mainnav animated fadeInLeft faster">
+            <div className="w-100 mt-3 text-right">
+              <i
+                className="fas fa-times mr-3 h4"
+                onClick={() => {
+                  this.setState({ subSideType: undefined });
+                  this.removeAllBgSilver();
+                }}
+              />
+            </div>
+            <h1>{this.state.subSideType}</h1>
+          </div>
+          <div
+            className="sidebar-sub-dimpart animated fadeIn slow"
+            onClick={() => {
+              this.setState({ subSideType: undefined });
+              this.removeAllBgSilver();
+            }}
+          />
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
       <div className="sidebar-container">
@@ -9,6 +94,8 @@ export default class SideBar extends Component {
           <img
             src={data.logoWithSlogan}
             className="img-fluid"
+            alt="web-logo"
+            onLoad={() => this.setState({ reloadImg: !this.state.reloadImg })}
           />
         </div>
         <div className="sidebar-bars">
@@ -16,16 +103,29 @@ export default class SideBar extends Component {
         </div>
         <div className="sidebar-icon-container mt-5 d-none d-lg-block">
           <div className="d-flex align-items-center flex-column h-100">
-            <div className="">
-              <i className="py-3 d-block fas fa-home" />
-              <i className="py-3 d-block far fa-calendar-check" />
+            <div className="mt-5">
+              <i
+                className="my-4 p-2 d-block fas fa-home"
+                onClick={(e) => this.handleClickIcon(e)}
+              />
+              <i
+                className="my-4 p-2 d-block far fa-calendar-check"
+                onClick={(e) => this.handleClickIcon(e)}
+              />
             </div>
-            <div className="py-3 mt-auto">
-              <i className="py-3 d-block fas fa-cog" />
-              <i className="py-3 d-block fas fa-question-circle" />
+            <div className="mt-4 mt-auto">
+              <i
+                className="my-4 p-2 d-block fas fa-cog"
+                onClick={(e) => this.handleClickIcon(e)}
+              />
+              <i
+                className="mt-3 p-2 d-block fas fa-question-circle"
+                onClick={(e) => this.handleClickIcon(e)}
+              />
             </div>
           </div>
         </div>
+        {this.renderSubSideBar()}
       </div>
     );
   }
---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+export const SIDE_HOME = "SIDE_HOME";
+export const SIDE_CALENDAR = "SIDE_CALENDAR";
+export const SIDE_SETTING = "SIDE_SETTING";
+export const SIDE_QUESTION = "SIDE_QUESTION";
