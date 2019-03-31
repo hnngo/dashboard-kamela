f63762005e0895f3f01291cc4cbe6b6ee1453b8e
Nhan <hnngo@ntu.edu.sg>
2019-03-31 17:38:03 +0800

    Adding react lib, d3 and other necessary stuffs

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello world
+      </div>
+    );
+  }
+}
+
+export default App;
