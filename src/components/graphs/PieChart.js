4fd232f051968930eb675df753b56d2223fb54c1
Nhan <hnngo@ntu.edu.sg>
2019-04-07 11:42:14 +0800

    Fixing sorting data on piechart

--- src/components/graphs/PieChart.js
+++ src/components/graphs/PieChart.js
@@ -11,7 +11,8 @@ export default class PieChart extends Component {
       totalWidth: 600,
       totalHeight: 200,
       svgInfo: undefined,
-      data: undefined
+      data: undefined,
+      toggleDraw: false,
     };
 
     this.drawData = this.drawData.bind(this);
@@ -26,6 +27,7 @@ export default class PieChart extends Component {
 
   componentWillReceiveProps(newProps) {
     // Check if pie chart receive new sort type props
+    // Then re-draw the data before rendering
     if (this.props.sortType !== newProps.sortType) {
       this.setState({ 
         data: _.countBy(this.props.data, (d) => d[newProps.sortType]) 
@@ -44,17 +46,18 @@ export default class PieChart extends Component {
                   .style("height", this.state.totalHeight)
                   .append("g")
                     .attr("transform", "translate(" + (maxRadius) + "," + (this.state.totalHeight/2) + ")");
-    
-    // Color scale
-    const colorScale = d3.scaleOrdinal(this.props.colorCode);
 
     // Init pie
-    const pie = d3.pie().value((d) => d)
+    const pie = d3.pie().value((d) => d).sort(null)
 
     // Init arc
     const arc = d3.arc()
                   .innerRadius(maxRadius - 40)
                   .outerRadius(maxRadius - 5);
+              
+    // Init legends
+    const legend = svg.append("g")
+    .attr("transform", "translate(" + (maxRadius + 20) + "," + (-this.state.totalHeight/2 + 20) + ")");
 
     // Tooltip
     const tip = d3.select('body')
@@ -62,7 +65,7 @@ export default class PieChart extends Component {
                   .attr('class', 'tooltip')
                   .style('opacity', 0);
     
-    const svgInfo = { svg, pie, arc, colorScale, tip, maxRadius };
+    const svgInfo = { svg, pie, arc, tip, maxRadius, legend };
 
     this.setState({ svgInfo }, () => this.drawData());
     // this.props.saveSVG(svgInfo, PIECHART_FS);
@@ -72,8 +75,7 @@ export default class PieChart extends Component {
     // console.log(this.props.sortType)
     // console.log("drawing datwa", this.state)
     const {
-      svg, pie, arc,
-      colorScale, tip, maxRadius
+      svg, pie, arc, legend, tip
     } = this.state.svgInfo;
 
     let data;
@@ -83,19 +85,25 @@ export default class PieChart extends Component {
       data = this.state.data;
     }
     console.log(data)
+    console.log(Object.keys(data))
+    console.log(Object.values(data))
+
+    // Color scale
+    const colorScale = d3.scaleOrdinal()
+                         .domain(Object.keys(data))
+                         .range(this.props.colorCode);
+
+    Object.keys(data).forEach((item) => console.log(`%c ${item}`, `background: ${colorScale(item)}`));
 
     // Calculating percentage for tooltips
     const sum = Object.values(data).reduce((acc, cur) => acc + cur);
     const percentageArr = Object.values(data).map((item) => item * 100 / sum);
 
     // Remove old legends
-    d3.selectAll(`.${this.props.chartName}circle`).remove();
-    d3.selectAll(`.${this.props.chartName}text`).remove();
+    legend.selectAll(`.${this.props.chartName}circle`).remove();
+    legend.selectAll(`.${this.props.chartName}text`).remove();
 
     // Add new legends
-    const legend = svg.append("g")
-                      .attr("transform", "translate(" + (maxRadius + 20) + "," + (-this.state.totalHeight/2 + 20) + ")");
-
     Object.keys(data).forEach((item, i) => {
       const legendRow = legend.append("g")
                               .attr("transform", "translate(0," + (i * 20) + ")");
@@ -105,11 +113,34 @@ export default class PieChart extends Component {
                .attr("cx", 0)
                .attr("r", 6)
                .attr("class", this.props.chartName + "circle")
-               .style("fill", colorScale(i))
+               .style("fill", colorScale(Object.keys(data)[i]))
                .attr("stroke", "#867979")
                .attr("stroke-width", 0.5)
-               .style("cursor", "pointer");
-      
+               .style("cursor", "pointer")
+               .on("click", () => {
+                console.log("index: " + i);
+                let notShow = [];
+
+                // Re-formating the circle fill color
+                let selectCircle = d3.select("." + this.props.chartName + "circle");
+                if (selectCircle.style("fill") === "white" || selectCircle.style("fill") === "rgb(0, 0, 0)") {
+                  // fill data
+                  selectCircle.style("fill", colorScale(i));
+
+                  // Update notShow
+                  // notShow = [...this.state.notShow];
+                  // notShow.splice(notShow.indexOf(i), 1);
+                } else {
+                  // un-fill data
+                  selectCircle.style("fill", "white");
+                  
+                  // Update notShow
+                  // notShow = [...this.state.notShow, i];
+                }
+                
+                this.setState({ toggleDraw: !this.state.toggleDraw });
+              });
+
       legendRow.append("text")
                 .attr("x", 10)
                 .attr("y", 6)
@@ -118,32 +149,44 @@ export default class PieChart extends Component {
                 .text(item)
     });
 
-
     // Draw data
     const path = svg.selectAll("path")
                     .data(pie(Object.values(data)));
-
+                 
+    // Update existing arcs
+    path.transition().duration(300).attrTween("d", arcTween)
+    
+    // Enter new arcs
     path.enter()
-       .append("path")
-       .attr("fill", (d, i) => colorScale(i))
-       .attr("d", arc)
-       .on("mousemove", (d, i) => {
-         // Tips showing
-         tip.transition().duration(200).style("opacity", .9);
-         tip.html(() => {
-            let res= `<svg height="15" width="15" style="margin: 10px 10px">`;
-            res += `<rect width="15" height="15" style="fill:${colorScale(i)};" />`;
-            res += `<span>${Object.keys(data)[i]}</span>`;
-            res += `<span style="margin-left:5px">${percentageArr[i]}%</span></svg>`;
+        .append("path")
+        .attr("fill", (d, i) => colorScale(Object.keys(data)[i]))
+        .attr("d", arc)
+        .attr("stroke", "white")
+        .attr("stroke-width", "4px")
+        .each(function(d) { this._current = d; })
+        .on("mousemove", (d, i) => {
+          // Tips showing
+          tip.transition().duration(200).style("opacity", .9);
+          tip.html(() => {
+             let res= `<svg height="15" width="15"  style="margin: 10px 10px">`;
+             res += `<rect width="15" height="15"  style="fill:${colorScale(i)};" />`;
+             res += `<span>${Object.keys(data)[i]}</span>`;
+             res += `<span style="margin-left:5px">${percentageArr[i]}%</span></svg>`;
+ 
+             return res;
+          }).style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 40) + "px");
+          })
+        .on("mouseout", (d) => {
+          // Tips dimming
+          tip.transition().duration(500).style("opacity", 0);
+        });
 
-            return res;
-         }).style("left", (d3.event.pageX) + "px")
-           .style("top", (d3.event.pageY - 40) + "px");
-         })
-       .on("mouseout", (d) => {
-         // Tips dimming
-         tip.transition().duration(500).style("opacity", 0);
-       })
+    function arcTween(a) {
+      const i = d3.interpolate(this._current, a);
+      this._current = i(1);
+      return (t) => arc(i(t));
+    }
   }
 
   render() {
