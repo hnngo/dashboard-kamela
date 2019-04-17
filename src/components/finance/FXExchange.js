dfe650a659deb5bcf6315ff9d2dd53fb4e997a19
Nhan <hnngo@ntu.edu.sg>
2019-04-18 00:26:22 +0800

    Adding collapsing currency range selection for FX

--- src/components/finance/FXExchange.js
+++ src/components/finance/FXExchange.js
@@ -8,8 +8,8 @@ export default class FXExchange extends Component {
     this.state = {
       widthThreshold: 640,
       slideShow: undefined,
-      resizeEvent: undefined
-
+      resizeEvent: undefined,
+      showCurrencySelection: false
     };
   }
 
@@ -39,41 +39,90 @@ export default class FXExchange extends Component {
     clearInterval(this.state.resizeEvent);
   }
 
+  handClickChangeCurrency(position) {
+    console.log(position);
+    this.setState({
+      showCurrencySelection: position
+    })
+  }
+
   renderConvertTable() {
     return (
       <div className="fxe-table-container">
         <div className="d-flex">
           <input
+            id="FXInputFrom"
             placeholder="1"
           />
-          <div className="fxe-table-country d-flex">
-            <img src="https://www.countryflags.io/be/shiny/32.png" alt="country-flag"/>
+          <div
+            className="fxe-table-country d-flex"
+            onClick={() => this.handClickChangeCurrency("from")}
+          >
+            <img src="https://www.countryflags.io/be/shiny/32.png" alt="country-flag" />
             <h6>USD</h6>
+            <i className="fas fa-chevron-down"></i>
           </div>
         </div>
-        {/* <div className="d-flex"> */}
-          <div className="fxe-ex-rate">
-            <ul>
-              <li>
-                <h6>Last Refresh:</h6>
-                <p>12 PM</p>
-              </li>
-              <li>
-                <h6>Exchange Rate:</h6>
-                <p>1.2323</p>
-              </li>
-            </ul>
-          </div>
-        {/* </div> */}
+        <div className="fxe-ex-rate">
+          <ul>
+            <li>
+              <h6>Last Refresh:</h6>
+              <p>12 PM</p>
+            </li>
+            <li>
+              <h6>Exchange Rate:</h6>
+              <p>1.2323</p>
+            </li>
+          </ul>
+        </div>
         <div className="d-flex">
           <input
             placeholder="1"
           />
-          <div className="fxe-table-country d-flex">
-            <img src="https://www.countryflags.io/be/shiny/32.png" alt="country-flag"/>
+          <div
+            className="fxe-table-country d-flex"
+            onClick={() => this.handClickChangeCurrency("to")}
+          >
+            <img src="https://www.countryflags.io/be/shiny/32.png" alt="country-flag" />
             <h6>USD</h6>
+            <i className="fas fa-chevron-down"></i>
           </div>
         </div>
+        {this.renderCurrencySelection()}
+      </div>
+    );
+  }
+
+  renderCurrencySelection() {
+    // Check if not click yet wont show info
+    if (!this.state.showCurrencySelection) {
+      return <div />;
+    }
+
+    const $curInput = document.querySelector("#FXInputFrom");
+    const $CCE = document.querySelector("#CCE");
+
+    // Assign the right style for selection from or to
+    let style = {};
+    if ($curInput && $CCE) {
+      style = {
+        top: $curInput.clientHeight + "px",
+        left: ($CCE.clientWidth - 300) / 2 + "px",
+        right: ($CCE.clientWidth - 300) / 2 + "px"
+      };
+
+      if (this.state.showCurrencySelection === "from") {
+        style.top = $curInput.clientHeight + "px";
+        style.bottom = "";
+      } else {
+        style.top = "";
+        style.bottom = ($curInput.clientHeight + 1) + "px";
+      }
+    }
+
+    return (
+      <div className="fxe-cs-container" style={style}>
+        <h5>Selection</h5>
       </div>
     );
   }
@@ -87,17 +136,13 @@ export default class FXExchange extends Component {
   }
 
   renderFXE1() {
-    return <div/>;
+    return <div />;
   }
 
   renderFXE2() {
     // Checking if slideshow needed for rendering
     if (this.state.slideShow) {
-      return (
-        <div>
-          {this.renderConvertTable()}
-        </div>
-      );
+      return this.renderConvertTable();
     } else {
       return (
         <div className="row">
@@ -136,3 +181,5 @@ export default class FXExchange extends Component {
     }
   }
 }
+
+//TODO: Add animated slide down or up
